71f666ca50320cfb3e32c85565cafecba2954cc8
Mykhailo Zezul <mzezul@getawaysresorts.com>
2024-04-16 16:23:31 -0700

    Initial commit

---
+++ Resonance/Program.cs
@@ -0,0 +1,22 @@
+﻿using Resonance.Services;
+using System.ComponentModel;
+
+namespace Resonance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? val = Console.ReadLine();
+
+            Unit unit1 = new Unit();
+
+            unit1.Value = val;
+
+            Console.WriteLine(unit1.Value);
+            Console.WriteLine(unit1.SIValue.ToString("0.##################################"));
+
+            Console.ReadLine();
+        }
+    }
+}
