e217564b3681838dc7183e7f9ac433caa887612a
Mykhailo Zezul <mzezul@getawaysresorts.com>
2024-04-19 12:24:09 -0700

    Added value swipper

--- Resonance/Program.cs
+++ Resonance/Program.cs
@@ -6,35 +6,58 @@ namespace Resonance
     {
         static void Main(string[] args)
         {
-
             Unit L = new Unit();
             Unit C = new Unit();
             Unit R = new Unit();
 
-            L.ParametricValue = "2m";
-            C.ParametricValue = "10u";
-            R.ParametricValue = "3";
+            L.ParametricValue = "2u";
+            C.ParametricValue = "100u";
+            R.SIValue = 0.01;
 
-            var lc = new LCSeries(R,L,C,null);
+            var lc = new LCSeries(R, L, C, null);
 
-            LCSeries.F_LC(lc);
-
-            Console.WriteLine("Q");
-            Console.WriteLine(lc.Q.SIValue);
-            Console.WriteLine("R");
-            Console.WriteLine(lc.R.SIValue);
-            Console.WriteLine("C");
-            Console.WriteLine(lc.C.SIValue);
-            Console.WriteLine("BW");
-            Console.WriteLine(lc.BW.SIValue);
-            Console.WriteLine("BW_FL");
-            Console.WriteLine(lc.BW_FL.SIValue);
-            Console.WriteLine("BW_FH");
-            Console.WriteLine(lc.BW_FH.SIValue);
-            Console.WriteLine("F");
-            Console.WriteLine(lc.F.SIValue);
-            Console.WriteLine("XL");
-            Console.WriteLine(lc.XL.SIValue);
+            var simulation = new UnitSweep(
+                new Unit() { ParametricValue="2u"},
+                new Unit() { ParametricValue="200u"},
+                new Unit() { ParametricValue="5u"}
+            );
+            
+            simulation.Sweep<LCSeries>(lc.L, LCSeries.F_LC, lc, Output);
+                               
+        }
+        public static void OutputAxises()
+        {
+            Console.Write("X ");
+            Console.Write("Q ");
+            Console.Write("R ");
+            Console.Write("C ");
+            Console.Write("BW ");
+            Console.Write("BW_FL ");
+            Console.Write("BW_FH ");
+            Console.Write("F ");
+            Console.Write("XL ");
+            Console.WriteLine();
+        }
+        public static void Output(LCSeries lc, double xAxis)
+        {
+            Console.Write(xAxis);
+            Console.Write(" ");
+            Console.Write(lc.Q.SIValue);
+            Console.Write(" ");
+            Console.Write(lc.R.SIValue);
+            Console.Write(" ");
+            Console.Write(lc.C.SIValue);
+            Console.Write(" ");
+            Console.Write(lc.BW.SIValue);
+            Console.Write(" ");
+            Console.Write(lc.BW_FL.SIValue);
+            Console.Write(" ");
+            Console.Write(lc.BW_FH.SIValue);
+            Console.Write(" ");
+            Console.Write(lc.F.SIValue);
+            Console.Write(" ");
+            Console.Write(lc.XL.SIValue);
+            Console.WriteLine();
         }
     }
 }
---
+++ Resonance/Services/UnitSweep.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resonance.Services
+{
+    public class UnitSweep
+    {
+        public Unit StartValue { get; set; }
+        public Unit StopValue { get; set; }
+        public Unit Step { get; set; }
+        public UnitSweep(Unit startValue, Unit stopValue, Unit step)
+        {
+            StartValue = startValue;
+            StopValue = stopValue;
+            Step = step;
+        }
+        public delegate void Iterator<T>(T circuit);
+        public delegate void Outputter<T>(T circuit, double stepValue);
+        public void Sweep<T>(Unit valueToSweep, Iterator<T> recalcFunction, T circuitInst, Outputter<T>? outputFunction)
+        {
+            double range = StopValue.SIValue - StartValue.SIValue;
+            int iterationsCount = (int)Math.Floor(range / Step.SIValue);
+            double sweeper = StartValue.SIValue;
+            for (int i = 0; i < iterationsCount; i++)
+            {
+                recalcFunction(circuitInst);
+                valueToSweep.SIValue = sweeper;
+                sweeper += Step.SIValue;
+                if (outputFunction != null)
+                {
+                    outputFunction(circuitInst, sweeper);
+                }
+            }
+        }        
+    }
+}
